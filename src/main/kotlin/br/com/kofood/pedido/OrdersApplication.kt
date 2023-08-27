cbe897f8b5352aad846278f2b62883fde56cecac
ocartaxo <oacartax@gmail.com>
2023-08-26 22:53:54 -0300

    rename

--- src/main/kotlin/br/com/kofood/pedido/OrdersApplication.kt
+++ src/main/kotlin/br/com/kofood/pedido/OrdersApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class PedidoApplication
+class OrdersApplication
 
 fun main(args: Array<String>) {
-    runApplication<PedidoApplication>(*args)
+    runApplication<OrdersApplication>(*args)
 }
